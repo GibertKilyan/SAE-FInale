81f564ab3fee0691409c204f162055de6f98de0a
Jarod Muneret <Jarod.Muneret@etu.univ-smb.fr>
2024-12-18 16:54:02 +0100

    se

--- TstSAE/MainWindow.xaml.cs
+++ TstSAE/MainWindow.xaml.cs
@@ -96,11 +96,11 @@ namespace TstSAE
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //charge la fonction Menu qui correspond a la window Menu//
-            FentreDemarrage();
+            FenetreDemarrage();
         }
         
         //Menu//
-        private void FentreDemarrage()
+        private void FenetreDemarrage()
         {
             this.Hide();
             Menu Menu = new Menu();
@@ -118,16 +118,21 @@ namespace TstSAE
             }
         
         }
+        
+        
+
+        
+
         private void Regles()
         {
             Regles Regles = new Regles();
             bool? regles = Regles.ShowDialog();
             if (regles == true)
-                FentreDemarrage();
+                FenetreDemarrage();
             if (regles == false)
             {
                 REGLEDUJEU = 0; // recharge le menu quand le bouton ok est cliquer
-                FentreDemarrage();
+                FenetreDemarrage();
             }
         }
 
@@ -618,7 +623,8 @@ namespace TstSAE
                     {
                         minuteurJeu.Stop();
                         chronoJeu.Stop();
-                        pause = true;
+                    pause = true;
+                        
                     }
                 }
         }
@@ -667,7 +673,7 @@ namespace TstSAE
                 minuteurJeu.Stop();
                 chronoJeu.Stop();
                 CanvaFond.Children.Clear();
-
+                
                 nbBouclier = 3;
                 nbScore = 0;
                 vitesseSpikeMan = 4;
@@ -677,6 +683,7 @@ namespace TstSAE
 
                 droite = false;
                 gauche = false;
+                lancer = false; 
                 pause = false;
                 accroupi = false;
                 lesSpikeMan.Clear();
